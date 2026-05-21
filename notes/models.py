dab9328c460941e5083b682f7e6b08dff18c48f7
SijanJ <sijan2joshi@gmail.com>
2026-05-21 16:23:26 +0545

    feat: add notesijan

--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,10 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+    
+class NoteSijan(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
